899de920605340110497290133a024ea98600638
Yury Popenko <popenko1337@gmail.com>
2023-11-06 19:26:54 +0700

    execute hello world example from docker container

--- producer/cmd/main.go
+++ producer/cmd/main.go
@@ -1,10 +1,58 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	_ "github.com/rabbitmq/amqp091-go"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"time"
+)
+
+const (
+	url string = "amqp://guest:guest@localhost:5672/"
 )
 
 func main() {
 	fmt.Println("hello from producer")
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		log.Fatalf("%s: %s\n", err, "failed to connect to RabbitMQ")
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("%s: %s\n", err, "failed to open a channel")
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("%s: %s\n", err, "failed to declare a queue")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "hello from producer"
+	err = ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	if err != nil {
+		log.Fatalf("%s: %s\n", err, "failed to publish a message")
+	}
+	log.Printf(" [x] Sent %s\n", body)
 }
